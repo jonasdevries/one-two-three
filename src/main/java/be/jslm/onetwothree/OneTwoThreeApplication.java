745c08e57951af595de760be2846c21794c5d710
Jonas Devries <jonas@good-it.be>
2018-11-13 08:47:04 +0100

    init

---
+++ src/main/java/be/jslm/onetwothree/OneTwoThreeApplication.java
@@ -0,0 +1,12 @@
+package be.jslm.onetwothree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTwoThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTwoThreeApplication.class, args);
+	}
+}
---
+++ src/test/java/be/jslm/onetwothree/OneTwoThreeApplicationTests.java
@@ -0,0 +1,16 @@
+package be.jslm.onetwothree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneTwoThreeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
